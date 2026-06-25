36f8d131e9b3d4ff72274abd284538baa4d4d723
tudaole <122336705+tudaole@users.noreply.github.com>
2026-06-25 11:09:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,21 +13,6 @@
 # 添加 adguardHome
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 
-# 添加 argon 主题
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-
-# 添加 Lucky
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-
-# 添加 netdata
-git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
-
-# 添加 oaf
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-
 # 添加 poweroffdevice
 git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
 
